7cd5e6c89bd35c9708a1af13b877ac69d1c8035f
amorbidoni <86751120+amorbidoni@users.noreply.github.com>
2022-10-26 10:35:35 -0300

    first commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+
+  constructor() {}
+  ngOnInit(): void {
+    let n = 10;
+    // debugger;  
+    n = 20;
+    n = n + 10;
+    n = 0;
+
+    console.log(n)
+  
+  }
+}
